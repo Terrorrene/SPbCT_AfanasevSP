622e3f440c70538dbf917e9f1e606e664c1666f2
Terrorrene <73238675+Terrorrene@users.noreply.github.com>
2020-11-06 00:39:36 +0300

    Add files via upload

---
+++ КР_АфанасьевСП/Control/Control.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <wchar.h>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, ".1251");
+	FILE *file;
+	char filnam[25] = "";
+	char str[50] = "";
+	cout << "Enter file name: "; cin >> filnam; //необходимо прописывать полный путь
+	fopen_s(&file,filnam,"r");
+	while ((fscanf_s(file, "%c", &str)) != EOF)
+	{
+		fgetwc(file);
+		printf("%s ", str);
+	}
+	fclose(file);
+	cout << endl;
+	system("Pause");
+}
+
